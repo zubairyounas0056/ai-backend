edb7c1e2a63fcef0dfa5537c0f1ab5ae74323ec1
zubairyounas0056 <zubairyounas0056@gmail.com>
2026-04-16 10:52:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1470,3 +1470,10 @@ def verify_user(data: dict):
         return {"status": "disabled"}
 
     return {"status": "ok", "timestamp": int(time.time())}
+
+port = int(os.environ.get("PORT", 8000))
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
