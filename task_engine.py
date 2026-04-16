51b0a7eefd0947a59562fdd1ba94cf84a602baa1
zubairyounas0056 <zubairyounas0056@gmail.com>
2026-04-16 10:30:58 +0500

    Add files via upload

---
+++ task_engine.py
@@ -0,0 +1,36 @@
+from concurrent.futures import ThreadPoolExecutor
+from human_simulator import HumanSimulator
+import threading
+import time
+import threading
+
+simulator = HumanSimulator()
+
+executor = ThreadPoolExecutor(max_workers=10)
+
+running_tasks = {}
+lock = threading.Lock()
+
+
+def run_parallel(task_id, func):
+    future = executor.submit(func)
+
+    with lock:
+        running_tasks[task_id] = future
+
+    return future
+
+
+def stop_task(task_id):
+    with lock:
+        future = running_tasks.get(task_id)
+        if future:
+            future.cancel()
+            del running_tasks[task_id]
+
+
+def stop_all_tasks():
+    with lock:
+        for task_id, future in running_tasks.items():
+            future.cancel()
+        running_tasks.clear()
