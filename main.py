c4209af1518604bde95c3a4ad8d6cdf584772081
zubairyounas0056 <zubairyounas0056@gmail.com>
2026-04-20 18:19:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1542,11 +1542,13 @@ def verify_user(data: dict):
 
 port = int(os.environ.get("PORT", 8000))
 
-if __name__ == "__main__":
-    # start_listener()
-    import uvicorn
+import uvicorn
+import os
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    start_listener()
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
 def check_expired_users():
